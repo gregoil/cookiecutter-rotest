c9682f64e6485c1e8ea127f3471f0edcf1990026
UnDarkle <edrafolt@gmail.com>
2019-10-10 12:36:02 +0300

    added imports and used django settings as config

--- hooks/post_gen_project.py
+++
@@ -1,5 +0,0 @@
-import os
-
-if __name__ == '__main__':
-    os.system("cd {{ cookiecutter.project_slug }}")
-    os.system("python manage.py migrate --settings={{ cookiecutter.project_slug }}.settings")
